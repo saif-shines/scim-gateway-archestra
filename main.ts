edeada0526f7d8f03e3a514f59df6ec1a5fd610a
saif-at-scalekit <saif.shaik@scalekit.com>
2026-02-11 13:23:34 +0530

    Sets up bareminimum with sdk resolutions

--- main.ts
+++ main.ts
@@ -1,8 +1,7 @@
+import { Scalekit } from "@scalekit-sdk/node";
+
+console.log(Scalekit)
+
 export function add(a: number, b: number): number {
   return a + b;
-}
-
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
 }
