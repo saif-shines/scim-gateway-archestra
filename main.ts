8195a0abb0c1fdcfdf137339698aa2eae1d6c798
saif-at-scalekit <saif.shaik@scalekit.com>
2026-02-11 14:37:26 +0530

    allow-net permission grant

--- main.ts
+++ main.ts
@@ -6,13 +6,12 @@ const app = new Hono()
 
 console.log(Scalekit)
 
-export function add(a: number, b: number): number {
-  return a + b;
-}
-console.log(add(12, 12));
-
 app.get('/', (c: Context) => c.text('Hono!'))
 
-app.post('/webhook', (c: Context) => c.text('Hono!'))
+app.post('/webhook', async (c: Context) => {
+  const body = await c.req.json();
+  console.log(body);
+  return c.text('Webhook received');
+})
 
 Deno.serve(app.fetch);
