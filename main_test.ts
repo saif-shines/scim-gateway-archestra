e0de0bac8d73655be6ebf3807c85e61d1450ee1f
saif-at-scalekit <saif.shaik@scalekit.com>
2026-02-11 22:32:01 +0530

    Remove main_test.ts file, which contained tests for supported event types in user lifecycle. This cleanup helps streamline the codebase by eliminating outdated test cases.

--- main_test.ts
+++
@@ -1,8 +0,0 @@
-import { assert } from "@std/assert";
-import { SUPPORTED_EVENT_TYPES } from "./models/syncTypes.ts";
-
-Deno.test("supported event types include user lifecycle", () => {
-  assert(SUPPORTED_EVENT_TYPES.includes("organization.directory.user_created"));
-  assert(SUPPORTED_EVENT_TYPES.includes("organization.directory.user_updated"));
-  assert(SUPPORTED_EVENT_TYPES.includes("organization.directory.user_deleted"));
-});
