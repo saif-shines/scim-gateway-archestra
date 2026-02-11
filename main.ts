512b82c5a96511dc0297cce8add29913696d72dc
saif-at-scalekit <saif.shaik@scalekit.com>
2026-02-11 13:44:18 +0530

    Install web server - hono

--- main.ts
+++ main.ts
@@ -1,7 +1,16 @@
 import { Scalekit } from "@scalekit-sdk/node";
+import { Hono, Context } from "@hono/hono";
+
+const app = new Hono()
+
 
 console.log(Scalekit)
 
 export function add(a: number, b: number): number {
   return a + b;
 }
+console.log(add(12, 12));
+
+app.get('/', (c: Context) => c.text('Hono!'))
+
+Deno.serve(app.fetch);
